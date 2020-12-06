ec00ae0334c30b29ecbdf4114ce46604fe5c65ac
webjazznl <hsiebelink73@gmail.com>
2020-12-06 18:11:33 +0100

    Build main header

--- hellogram/resources/views/home.blade.php
+++ hellogram/resources/views/home.blade.php
@@ -2,21 +2,24 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5"><img class="rounded-circle" src="https://scontent-ams4-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_ohc=qFciZzYef_0AX9MDNTD&tp=1&oh=972c9c4273d02f7ca6152b4a2c1f1d16&oe=5FF4D467" alt=""></div>
+        <div class="col-9">
+            <div class="pt-5">
+                <h1>helloGram</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>25K</strong> followers</div>
+
+                <div class="pr-5"><strong>1111</strong> following</div>
+
+            </div>
+            <div class="pt-4" style="font-weight: bold;">helloGram.org</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Sequi quasi corrupti facilis? Neque atque, est officiis
+                porro perferendis necessitatibus animi!</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
         </div>
     </div>
 </div>
