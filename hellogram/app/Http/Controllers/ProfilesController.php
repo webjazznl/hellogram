beada1ac9cc8986902b937b2f3ec470cafe2ad10
webjazznl <hsiebelink73@gmail.com>
2020-12-08 21:35:50 +0100

    Create Profilescontroler and view

---
+++ hellogram/app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function show(User $user)
+    {
+        return view('home', ['username' => $user->username,]);
+    }
+}
--- hellogram/resources/views/home.blade.php
+++ hellogram/resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-3 p-5"><img class="rounded-circle" src="https://scontent-ams4-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_ohc=qFciZzYef_0AX9MDNTD&tp=1&oh=972c9c4273d02f7ca6152b4a2c1f1d16&oe=5FF4D467" alt=""></div>
         <div class="col-9">
             <div class="pt-5">
-                <h1>helloGram</h1>
+                <h1>{{$username}}</h1>
             </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
