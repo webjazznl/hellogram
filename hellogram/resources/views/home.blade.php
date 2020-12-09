7dda059424654d5ffcf5359c7b907550d78472e6
webjazznl <hsiebelink73@gmail.com>
2020-12-09 21:10:34 +0100

    Display url in profiile

--- hellogram/resources/views/home.blade.php
+++ hellogram/resources/views/home.blade.php
@@ -17,7 +17,7 @@
             </div>
             <div class="pt-4" style="font-weight: bold;">{{$user->profile->title}}</div>
             <div>{{$user->profile->description}}</div>
-            <div><a href="#">{{$user->profile->url}}</a></div>
+            <div><a href="{{$user->profile->url}}">{{$user->profile->url}}</a></div>
         </div>
     </div>
     <div class="row pt-4">
